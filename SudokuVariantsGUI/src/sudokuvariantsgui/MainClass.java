05da1c0495d63880f10f54585dfb6e4e27b38063
Jacob Coring <chf6534@autuni.ac.nz>
2022-10-14 16:37:07 +1300

    Created Menus

---
+++ SudokuVariantsGUI/src/sudokuvariantsgui/MainClass.java
@@ -0,0 +1,12 @@
+package sudokuvariantsgui;
+
+/**
+ * Project: SudokuVariantsGUI
+ * @author Jacob Coring
+ */
+public class MainClass {
+    
+    public static void main(String[] args) {
+        new Main().setVisible(true);
+    }
+}
