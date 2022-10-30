b79ab3eea935c8bfa1365f07f853b31c8669a5d3
Jacob Coring <chf6534@autuni.ac.nz>
2022-10-31 07:23:57 +1300

    Final Program

--- SudokuVariantsGUI/src/sudokuvariantsgui/SudokuVariantsGUI.java
+++ SudokuVariantsGUI/src/sudokuvariantsgui/SudokuVariantsGUI.java
@@ -9,21 +9,16 @@ import javax.swing.UIManager;
 public class SudokuVariantsGUI {
     
     public SudokuVariantsGUI() {
-        new SudokuVariantsController(new SudokuVariantsView(),new SudokuVariants());
+        SudokuVariantsView view = new SudokuVariantsView();
+        SudokuVariants model = new SudokuVariants();
+        new SudokuVariantsController(view,model);
     }
     
     public static void main(String[] args) {
         
         try {
             UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
-        } catch (ClassNotFoundException ex) {
-            //java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            //java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            //java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            //java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException e) {
         }
 
         new SudokuVariantsGUI();
